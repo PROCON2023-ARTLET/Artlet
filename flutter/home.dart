52f4a7e3560c07fb67b06b5576a4c531d39fa722
Myagmarsuren014 <145972563+Myagmarsuren014@users.noreply.github.com>
2023-10-08 16:13:38 +0800

    Update home.dart

--- flutter/home.dart
+++ flutter/home.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: depend_on_referenced_packages
-
 import 'package:flutter/material.dart';
-import 'package:application/score_page.dart';
 import 'package:application/turshilt_page.dart';
 
 void main() => runApp(const HomePage());
@@ -19,131 +16,50 @@ class HomePage extends StatelessWidget {
 }
 
 class CombinedScreen extends StatelessWidget {
-  void onScoringScreen(BuildContext context, String path) {
-    switch (path) {
-      case "mongol":
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => DrawingScreen()),
-        );
-        break;
-      case "nihongo":
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: ((context) => DrawingScreen())),
-        );
-        break;
-      case "check":
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: ((context) => ScoreApp())),
-        );
-        break;
-      default:
-    }
+  void onPlayButtonPressed(BuildContext context) {
+    // Navigate to the DrawingScreen when the "Play" button is pressed
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => DrawingScreen()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Language Selector'),
-        backgroundColor: Colors.blue, // Customize the app bar color
+        title: Text('Play Button'),
+        backgroundColor: Colors.blue,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                onScoringScreen(context, "mongol");
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue, // Button background color
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0), // Add padding
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.language,
-                      color: Colors.white, // Icon color
-                      size: 24.0, // Set the icon size
-                    ),
-                    SizedBox(width: 10), // Add spacing between icon and text
-                    Text(
-                      'Монгол',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white, // Text color
-                      ),
-                    ),
-                  ],
+        child: ElevatedButton(
+          onPressed: () {
+            onPlayButtonPressed(context);
+          },
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  Icons.play_arrow,
+                  color: Colors.white,
+                  size: 24.0,
                 ),
-              ),
-            ),
-            SizedBox(height: 16), // Add spacing between buttons
-            ElevatedButton(
-              onPressed: () {
-                onScoringScreen(context, "nihongo");
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green, // Customize button color
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.language,
-                      color: Colors.white,
-                      size: 24.0,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      '日本語',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
+                SizedBox(width: 10),
+                Text(
+                  'Play',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
+              ],
             ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                onScoringScreen(context, "garah");
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red, // Customize button color
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.exit_to_app,
-                      color: Colors.white,
-                      size: 24.0,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      'QUIT',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
